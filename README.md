a48738494d9e3d7954be2e616c3e366432ebff9e
EthanWickenkamp <102557780+EthanWickenkamp@users.noreply.github.com>
2026-01-07 13:58:16 -0600

    scaffold
    start

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Hardhat-test
-trying hardhat for first time
+# Sample Hardhat 3 Beta Project (minimal)
+
+This project has a minimal setup of Hardhat 3 Beta, without any plugins.
+
+## What's included?
+
+The project includes native support for TypeScript, Hardhat scripts, tasks, and support for Solidity compilation and tests.
+
+
+
+##### package.json
+npm package file
+hardhat and plugin packages installed
+
+##### tsconfig.json
+type script config
+
+##### hardhat.config.ts
+hardhat project main config file
+define solidity compiler version, plugin, tasks
+
+##### contracts
+contains soliditiy contracts, .sol files, .t.sol test files
+
+##### tests
+typescript of solidity tests
+.t.sol is optional any .sol works in this folder
+
+##### ignition
+deployment modules to deploy contacts to blockchain
+separate hardhat ignition section
+
+##### scripts
+custom scripts to automate workflow
+full access to hardhat runtime 
+can use plugins, connect to networks, and deploy contracts
---
+++ hardhat.config.ts
@@ -0,0 +1,54 @@
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import { configVariable, defineConfig } from "hardhat/config";
+
+export default defineConfig({
+  plugins: [hardhatToolboxViemPlugin],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.28",
+      },
+      production: {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+  },
+});
+
+
+// gpt amoy network connection 
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    amoy: {
+      url: process.env.AMOY_RPC_URL,
+      chainId: 80002,
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+    },
+  },
+};
